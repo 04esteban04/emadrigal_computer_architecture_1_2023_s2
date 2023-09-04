9438914b85258ce58079234d544eeaa188abb0e4
04esteban04 <estebanmadrigal7@gmail.com>
2023-09-04 01:51:41 -0600

    Agregando funcion sen(x)

--- assembly/factorial.s
+++ assembly/factorial.s
@@ -2,32 +2,41 @@
 .global factorial           @ Define la función global "factorial"
 
 factorial:
-    push {r3, r4}           @ Guarda los registros r3 y r4 en la pila antes de usarlos
-    mov r2, r1              @ Copia el contenido del registro r1 en r2 y r3 (uso posterior)
-    mov r3, r1
+    push {r0}               @ Guarda los registros en la pila antes de usarlos
+    mov r0, r2              @ Copia el contenido del registro r2 (2n+1) en r0 y r1 (uso posterior)
     
-    cmp r2, #1              @ Compara el valor en r2 con 1
-    beq factorial_exit      @ Si r1 es igual a 1, salta a la etiqueta factorial_exit
+    cmp r0, #1              @ Compara el valor en r0 con 1
+    beq factorial_casoBase  @ Si r0 es igual a 1, salta a la etiqueta factorial_casobase1
 
-    cmp r2, #0              @ Compara el valor en r2 con 0
-    beq factorial_baseCase  @ Si r2 es igual a 0, salta a la etiqueta factorial_baseCase
+    cmp r0, #0              @ Compara el valor en r0 con 0
+    beq factorial_casoBase @ Si r0 es igual a 0, salta a la etiqueta factorial_casoBase2
+
+    vmov s2, r0             @ Cargar r0 en s2 
+    vcvt.f32.s32 s2, s2     @ Convertir a float
 
 factorial_loop:
-    sub r3, r3, #1          @ Decrementa el contador en r3 (r3 = r3 - 1)
-    mul r4, r2, r3          @ Multiplica el total acumulado y el contador (r2 * r3), y guarda en r4
-    mov r2, r4              @ Copia el resultado de la multiplicación a r2
-    
-    cmp r3, #1              @ Compara el contador con 1
-    beq factorial_exit      @ Si el resultado de la comparación es "igual" (r3 == 1), salta a factorial_exit
+    sub r0, r0, #1          @ Decrementa el contador en r0 (r0 = r0 - 1)
+
+    vmov s1, r0             @ Cargar r2 en s2 (s0 = 2n+1)
+    vcvt.f32.s32 s1, s1     @ Convertir a float
+
+    vmul.f32 s0, s2, s1     @ Multiplica el total acumulado y el contador (s2 * s1), y guarda en s0
+    vmov s2, s0             @ Copia el resultado de la multiplicación a s2
+
+    cmp r0, #1              @ Compara el contador con 1
+    beq factorial_exit      @ Si el resultado de la comparación es "igual" (r0 == 1), salta a factorial_exit
     
     b factorial_loop        @ Salta a factorial_loop para hacer otra iteración
                             @ Continúa hasta que el contador sea 0
 
-factorial_baseCase:
-    @ Caso base (factorial de 0)
-    mov r2, #1              @ Establece r2 en 1
+factorial_casoBase:
+    @ Caso base (factorial de 0 o factorial de 1)
+    mov r0, #1
+    vmov s0, r0             @ Cargar r2 en s2 (s0 = 2n+1)
+    vcvt.f32.s32 s0, s0     @ Convertir a float
+
     b factorial_exit        @ Salta a factorial_exit
 
 factorial_exit:
-    pop {r3, r4}            @ Restaura los registros r3 y r4 desde la pila
+    pop {r0}                @ Restaura los registros desde la pila
     bx lr                   @ Devuelve el control a la función que llamó a factorial
--- assembly/potencia.s
+++ assembly/potencia.s
@@ -2,37 +2,45 @@
 .global potencia           @ Define la función global "potencia"
 
 potencia:
-    push {r2, r5}           @ Guarda los registros en la pila antes de usarlos
-    mov r2, r3              @ Copia el contenido del registro r3 en r2 (base)
-    mov r5, r1              @ Copia el valor de r1 a r5 (potencia actual)
+    push {r0, r1}       @ Guarda los registros en la pila antes de usarlos
+    mov r1, r2              @ Copia el valor de la potencia actual (2n+1)
     
-    cmp r5, #0              @ Compara el valor en r5 con 0 (potencia 0)
+    cmp r1, #0              @ Compara el valor en r5 con 0 (potencia 0)
     beq potencia_casoBase0  @ Si r5 es igual a 0, salta a la etiqueta potencia_casoBase0
 
-    cmp r5, #1              @ Compara el valor en r5 con 1 (potencia 1)
+    cmp r1, #1              @ Compara el valor en r5 con 1 (potencia 1)
     beq potencia_casoBase1  @ Si r5 es igual a 1, salta a la etiqueta potencia_casoBase1
 
-potencia_loop:
-    sub r5, r5, #1          @ Decrementa el contador en r5 (r5 = r5 - 1)
-    mul r4, r2, r3          @ Multiplica el total acumulado y el contador (r2 * r2), y guarda en r4
-    mov r2, r4
+    vmov s1, s8             @ Cargar s8 en s0 (base)
+    vmov s2, s8             @ Cargar s8 en s1 (base)
 
-    cmp r5, #1              @ Compara el contador con 1
-    beq potencia_exit       @ Si el resultado de la comparación es "igual" (r5 == 1), salta a potencia_exit
+
+potencia_loop:
+    sub r1, r1, #1          @ Decrementa el contador en r1 (r1 = r1 - 1)
+
+    vmul.f32 s3, s1, s2     @ Multiplica el total acumulado y el contador (s2 * s1), y guarda en s0
+    vmov s1, s3             @ Copia el resultado de la multiplicación a s2
+
+    cmp r1, #1              @ Compara el contador con 1
+    beq potencia_exit       @ Si el resultado de la comparación es "igual" (r1 == 1), salta a potencia_exit
     
     b potencia_loop         @ Salta a potencia_loop para hacer otra iteración
                             @ Continúa hasta que el contador sea 0
 
 potencia_casoBase0:
     @ Caso base (potencia de 0)
-    mov r4, #1              @ Establece r4 en 1
+    mov r0, #1              @ Establece r0 en 1
+    vmov s1, r0             @ Cargar r0 en s1 (s1 = x^(2n+1))
+    vcvt.f32.s32 s1, s1     @ Convertir a float
+
     b potencia_exit         @ Salta a potencia_exit
 
 potencia_casoBase1:
     @ Caso base (potencia de 1)
-    mov r4, r2              @ Establece r4 en r2 (base)
+    vmov s1, s8             @ Cargar r0 en s1 (s1 = x^(2n+1))
+
     b potencia_exit         @ Salta a potencia_exit
 
 potencia_exit:
-    pop {r2, r5}            @ Restaura los registros desde la pila
+    pop {r0, r1}            @ Restaura los registros desde la pila
     bx lr                   @ Devuelve el control a la función que llamó a potencia
